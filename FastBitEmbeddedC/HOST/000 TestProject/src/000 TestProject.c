19140763ae3085bbcfb8c5e095de7360f5bc7767
Diego De Niro <deniro.diego@gmail.com>
2021-04-11 08:22:32 -0300

    Module 02 - Lecture 10 - Test Program

---
+++ FastBitEmbeddedC/HOST/000 TestProject/src/000 TestProject.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : 000.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	getchar();
+	return EXIT_SUCCESS;
+}
